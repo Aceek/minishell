32f361b71abc2b8f7a69f2444420376adcfd394a
Ilan LINHARDT <ilinhard@e2r7p23.clusters.42paris.fr>
2022-10-28 06:24:33 +0200

    first exec version (need redirect pipe clear) based on other minishell parsing/lexer

---
+++ src/new_exec/exec.c
@@ -0,0 +1,116 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilinhard <ilinhard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/28 00:19:10 by ilinhard          #+#    #+#             */
+/*   Updated: 2022/10/28 06:19:51 by ilinhard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+int	ft_count_list(t_data *data)
+{
+	int	i;
+	t_data *tmp;
+
+	tmp = data;
+	i = 0;
+	while (tmp)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	return (i);
+}
+
+
+
+int	ft_cmd(t_data *data)
+{
+	char	*path;
+
+	if (data->new_args[0][0] == '/')
+		path = data->new_args[0];
+	else
+		path = ft_get_path(data->new_args[0], data->env);	
+	if (!path)
+	{
+		printf("%s : command not found\n", data->new_args[0]);
+		free_tab(data->new_args);
+		return (-1); // a changer par un return 
+	}
+	execve(path, data->new_args, data->env);
+	free_tab(data->new_args);
+	printf("can't access command\n");
+	return (-1);
+	
+}
+
+int	ft_fork(t_env *mini, t_data *data)
+{
+	int	fd[2];
+	int	pid;
+	(void)mini;
+
+	pipe(fd);
+	pid = fork();
+	if (pid != 0)
+	{
+		close(fd[1]);
+		dup2(fd[0], STDIN_FILENO);
+		wait(NULL);
+		close(fd[0]);
+	}
+	else if (pid == 0)
+	{
+		close(fd[0]);
+		dup2(fd[1], STDOUT_FILENO);
+		ft_cmd(data);
+		return (close(fd[1]), -1);
+	}
+	return (0);
+}
+
+int	ft_last_child(t_data *data)
+{
+	int	pid;
+	pid = fork();
+	if (pid == 0)
+	{
+	if (data->out > 0)
+		dup2(data->out, STDOUT_FILENO);
+	ft_cmd(data);
+	}
+	else
+	{
+		wait(NULL);
+	if (data->out > 0)
+		dup2(STDOUT_FILENO, data->out);
+	}
+	return (0);
+}
+
+void	ft_exe(t_env *mini, t_env *origin, t_data *data)
+{
+	(void)origin;
+	(void)mini;
+
+	t_data *tmp;
+
+	tmp = data;
+	while (tmp && ft_count_list(tmp) >= 2)
+	{
+		if (ft_fork(mini, tmp) == -1)
+		{
+			printf("error tmp fork\n\n");		
+			return ;
+		}
+		tmp = tmp->next;
+	}
+	ft_last_child(tmp);
+
+}
---
+++ src/new_exec/utils.c
@@ -0,0 +1,146 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilinhard <ilinhard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/22 13:42:35 by ilinhard          #+#    #+#             */
+/*   Updated: 2022/10/28 06:05:21 by ilinhard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+char	*ft_strnstr(const char *s1, const char *to_find, size_t n)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (*to_find == '\0' || to_find == NULL)
+		return ((char *)s1);
+	while (s1[i] && i < n)
+	{
+		j = 0;
+		while (s1[i + j] == to_find[j] && j + i < n)
+		{
+			j++;
+			if (to_find[j] == '\0')
+				return ((char *)&s1[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+void	free_tab(char **av)
+{
+	int	i;
+
+	i = 0;
+	while (av && av[i] != NULL)
+	{
+		free(av[i]);
+		i++;
+	}
+	free(av);
+}
+
+char	*ft_make_path(char *dir, char *cmd)
+{
+	int		i;
+	int		j;
+	char	*path;
+
+	i = 0;
+	j = 0;
+	path = malloc(sizeof(char) * (ft_strlen(cmd) + ft_strlen(dir) + 2));
+	if (!path)
+		return (NULL);
+	while (dir[i])
+	{
+		path[i] = dir[i];
+		i++;
+	}
+	path[i] = '/';
+	i++;
+	while (cmd[j])
+	{
+		path[i + j] = cmd[j];
+		j++;
+	}
+	path[i + j] = '\0';
+	return (path);
+}
+
+char	*ft_cpy(char *src, int skip)
+{
+	int		i;
+	int		max_len;
+	char	*dst;
+
+	i  = 0;
+	while (src[i])
+		i++;
+	max_len = i - skip;
+	dst = malloc(sizeof(char) * (i - skip + 1));
+	if (!dst)
+		return (NULL);
+	i = 0;
+	while (src && src[i] && i < max_len)
+	{
+		dst[i] = src[i + skip];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
+char	*ft_get_path(char *cmd, char **env)
+{
+	char	*path;
+	char	**all_dir;
+	char	*tmp_path;
+	int		i;
+
+	i = 0;
+	while (env[i] && ft_strnstr(env[i], "PATH=", 5) == NULL)
+		i++;
+	if (!env[i])
+		return (NULL);
+	tmp_path = ft_cpy(env[i], 5);
+	all_dir = ft_split2(tmp_path, ':');
+	i = 0;
+	while (all_dir[i] != NULL)
+	{
+		path = ft_make_path(all_dir[i], cmd);
+		if (!path)
+			return (free(tmp_path), free(all_dir), NULL);
+		if (access(path, F_OK) == 0)
+			return (free(tmp_path), free_tab(all_dir), path);
+		free (path);
+		i++;
+	}
+	return (free(tmp_path), free_tab(all_dir), NULL);
+}
+
+int	ft_error(int error)
+{
+	if (error == -1)
+	{
+		write(STDERR_FILENO, "Wrong args nunbers\n", 19);
+		return (-1);
+	}
+	else if (error == -2)
+	{
+		write(STDERR_FILENO, "Can't open file\n", 16);
+		return (-2);
+	}
+	else if (error == -3)
+	{
+		write(STDERR_FILENO, "Can't acces file\n", 17);
+		return (-3);
+	}
+	return (0);
+}
