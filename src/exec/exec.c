36155b93acb778b272b7f2c4c1ca71fe8f1c5796
aceek <linhardtilan@gmail.com>
2022-11-26 23:23:29 +0100

    merge

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: ilinhard <ilinhard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 00:19:10 by ilinhard          #+#    #+#             */
-/*   Updated: 2022/11/26 07:08:38 by ilinhard         ###   ########.fr       */
+/*   Updated: 2022/11/26 23:23:14 by ilinhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int	ft_cmd(t_cmd *cmd, t_env *mini)
 	else if (!cmd->builtin)
 		path = ft_get_path(cmd->tab[0], cmd->env);
 	if ((cmd->fd_in < 0 || cmd->fd_out < 0) || (!path && !cmd->builtin))
-		return (free(path), -2); // if fd < 0 alors on stop le prob dans l'initialisation
+		return (free(path), -2);
 	if (cmd->fd_in > STDIN_FILENO)
 		dup2(cmd->fd_in, STDIN_FILENO);
 	if (cmd->fd_out > STDOUT_FILENO)
@@ -116,6 +116,8 @@ void	ft_exe(t_env *mini, t_cmd *cmd)
 	int		in;
 	t_cmd	*tmp;
 
+	if (!cmd)
+		return ;
 	in = dup(0);
 	out = dup(1);
 	tmp = cmd;
@@ -136,6 +138,4 @@ void	ft_exe(t_env *mini, t_cmd *cmd)
 	if (ft_last_child(tmp, mini) < 0)
 		ft_exit_clean(mini, cmd, 127);
 	ft_close_and_reset_exec(cmd, out, in);
-
-	// si la derniere exec est un file or directory existe pas alors g_exit = 1;
 }
