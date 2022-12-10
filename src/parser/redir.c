83cb54d2ecf5df013f9bca7f650a53135f79a7e3
Pierre BEHEYT <pbeheyt@e2r1p4.clusters.42paris.fr>
2022-12-10 05:52:13 +0100

    add ft cpy no quotes + new token args parsing

--- src/parser/redir.c
+++ src/parser/redir.c
@@ -6,48 +6,23 @@
 /*   By: pbeheyt <pbeheyt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 03:00:22 by pbeheyt           #+#    #+#             */
-/*   Updated: 2022/12/10 04:48:20 by pbeheyt          ###   ########.fr       */
+/*   Updated: 2022/12/10 05:36:33 by pbeheyt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-// char	*get_redir_arg(t_data *data, char *str, int *i)
-// {
-// 	char	*buf;
-// 	char	*dup;
-
-// 	buf = create_buffer();
-// 	if (!buf)
-// 		return (free_all_exit(data, 1), NULL);
-// 	while (is_del(str, *i))
-// 		*i += 1; 
-// 	while (!is_del(str, *i))
-// 	{
-// 		if (data->token == DLESS)
-// 			buf = add_char(data, buf, str[*i]);
-// 		else
-// 			buf = convert_input(data, buf, str, i);
-// 	*i += 1;
-// 	}
-// 	dup = ft_strdup(buf);
-// 	dup = cpy_no_quotes(dup, buf, 0, ft_strlen(dup));
-// 	free(buf);
-// 	printf("arg : %s\n", dup);
-// 	return(dup);
-// }
-
 char	*get_redir_arg(t_data *data, char *str, int *i)
 {
 	char	*buf;
+	char	*dup;
 
 	buf = create_buffer();
 	if (!buf)
 		return (free_all_exit(data, 1), NULL);
-	while (ft_isspace(str[*i]))
-		*i += 1;
-	while (ft_isalnum(str[*i]) || str[*i] == '_' || str[*i] == '.'
-		|| str[*i] == '$' || str[*i] == '/')
+	while (is_del(str, *i))
+		*i += 1; 
+	while (!is_del(str, *i))
 	{
 		if (data->token == DLESS)
 			buf = add_char(data, buf, str[*i]);
@@ -55,7 +30,10 @@ char	*get_redir_arg(t_data *data, char *str, int *i)
 			buf = convert_input(data, buf, str, i);
 	*i += 1;
 	}
-	return (buf);
+	dup = ft_strdup(buf);
+	dup = cpy_no_quotes(dup, buf, 0, ft_strlen(dup));
+	free(buf);
+	return(dup);
 }
 
 int	redir_handler(t_data *data, char *str, int *i)
